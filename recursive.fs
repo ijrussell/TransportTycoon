57e82d381d8e6eca16294c956fe0543e2ffb4f2e
Ian Russell <ijrussell@fastmail.co.uk>
2019-11-08 19:05:03 +0000

    Added initial version

---
+++ recursive.fs
@@ -0,0 +1,54 @@
+module Recursive
+
+type Destination = Factory | Port | A | B
+type Journey = Destination * int
+type State = int * Destination list * Destination list * Journey * Journey * Journey
+
+let evaluate (input:Journey) =
+    let (d, a) = input
+    match d with
+    | A | B -> Some a
+    | _ -> None
+
+let getMaxValue items =
+    match (items |> List.choose evaluate) with
+    | [] -> 0
+    | list -> list |> List.max
+
+let processTruck (truck:Journey) cd cargo portCargo =
+    match truck with
+    | (d, a) when d = Factory && a = cd -> 
+        match cargo with
+        | A::tail -> ((Port, cd + 1), tail, portCargo) 
+        | B::tail -> ((B, cd + 5), tail, portCargo) 
+        | _ -> ((d, a), cargo, portCargo) 
+    | (d, a) when d = Port && a = cd -> ((Factory, cd + 1), cargo, A::portCargo)
+    | (d, a) when d = B && a = cd -> ((Factory, cd + 5), cargo, portCargo)
+    | _ -> (truck, cargo, portCargo)
+
+let processShip (ship:Journey) cd portCargo =
+    match ship with
+    | (d, a) when d = Port && a <= cd -> 
+        match portCargo with
+        | A::tail -> ((A, cd + 4), tail) 
+        | _ -> ((d, cd), portCargo)
+    | (d, a) when d = A && a = cd -> ((Port, cd + 4), portCargo) 
+    | _ -> (ship, portCargo)
+
+let rec tick state result =
+    let (i, cargo, portCargo, truck1, truck2, ship) = state
+    if cargo <> [] || portCargo <> [] then
+        let (truck1', cargo', portCargo') = (i, cargo, portCargo) |||> processTruck truck1 
+        let (truck2', cargo'', portCargo'') = (i, cargo', portCargo') |||> processTruck truck2
+        let (ship', portCargo''') = (i, portCargo'') ||> processShip ship 
+        //printfn "Day: %A - T1: %A - T2: %A - S: %A - Cargo: %A - PortCargo: %A" cd truck1' truck2' ship' cargo'' portCargoX
+        let maxValue = [ truck1'; truck2'; ship'; (A, 0) ] |> getMaxValue
+        tick (i + 1, cargo'', portCargo''', truck1', truck2',ship') maxValue
+    else 
+        result
+
+let run cargo =
+    tick (0, cargo, [], (Factory, 0), (Factory, 0), (Port, 0)) 0
+
+run [A;A;B;A;B;B;A;B] |> printfn "%A" 
+
