7f6303260d9d365e15b2572bdc58177517e83804
Ian Russell <ijrussell@fastmail.co.uk>
2019-11-10 12:06:58 +0000

    Removed result from rec call

--- recursive.fs
+++ recursive.fs
@@ -21,7 +21,7 @@ let processTruck (truck:Journey) cd cargo portCargo =
         match cargo with
         | A::tail -> ((Port, cd + 1), tail, portCargo) 
         | B::tail -> ((B, cd + 5), tail, portCargo) 
-        | _ -> ((d, a), cargo, portCargo) 
+        | _ -> (truck, cargo, portCargo) 
     | (d, a) when d = Port && a = cd -> ((Factory, cd + 1), cargo, A::portCargo)
     | (d, a) when d = B && a = cd -> ((Factory, cd + 5), cargo, portCargo)
     | _ -> (truck, cargo, portCargo)
@@ -35,20 +35,19 @@ let processShip (ship:Journey) cd portCargo =
     | (d, a) when d = A && a = cd -> ((Port, cd + 4), portCargo) 
     | _ -> (ship, portCargo)
 
-let rec tick state result =
+let rec tick state =
     let (i, cargo, portCargo, truck1, truck2, ship) = state
     if cargo <> [] || portCargo <> [] then
         let (truck1', cargo', portCargo') = (i, cargo, portCargo) |||> processTruck truck1 
         let (truck2', cargo'', portCargo'') = (i, cargo', portCargo') |||> processTruck truck2
         let (ship', portCargo''') = (i, portCargo'') ||> processShip ship 
-        //printfn "Iteration: %A - T1: %A - T2: %A - S: %A - Cargo: %A - PortCargo: %A" i truck1' truck2' ship' cargo'' portCargo'''
-        let maxValue = [ truck1'; truck2'; ship' ] |> getMaxValue
-        tick (i + 1, cargo'', portCargo''', truck1', truck2', ship') maxValue
+        printfn "Iteration: %A - T1: %A - T2: %A - S: %A - Cargo: %A - PortCargo: %A" i truck1' truck2' ship' cargo'' portCargo'''
+        tick (i + 1, cargo'', portCargo''', truck1', truck2', ship')
     else 
-        result
+        [ truck1; truck2; ship ] |> getMaxValue
 
 let run cargo =
-    tick (0, cargo, [], (Factory, 0), (Factory, 0), (Port, 0)) 0
+    tick (0, cargo, [], (Factory, 0), (Factory, 0), (Port, 0))
 
 run [A;A;B;A;B;B;A;B] |> printfn "%A" 
 
